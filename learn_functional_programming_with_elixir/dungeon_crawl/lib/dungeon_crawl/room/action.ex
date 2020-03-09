94ab54338e29d87ddbc0b9da062d61644d5a7de0
Alexandre Moreira Xavier <alexandrexaviersm@gmail.com>
2020-03-09 03:48:21 -0300

    Create enemies/rooms and battles for the game

--- learn_functional_programming_with_elixir/dungeon_crawl/lib/dungeon_crawl/room/action.ex
+++ learn_functional_programming_with_elixir/dungeon_crawl/lib/dungeon_crawl/room/action.ex
@@ -1,6 +1,13 @@
 defmodule DungeonCrawl.Room.Action do
+  @type t :: %__MODULE__{id: atom, label: String.t()}
+
   defstruct label: nil, id: nil
+
   def forward, do: %__MODULE__{id: :forward, label: "Move forward."}
   def rest, do: %__MODULE__{id: :rest, label: "Take a better look and rest."}
   def search, do: %__MODULE__{id: :search, label: "Search the room."}
+
+  defimpl String.Chars do
+    def to_string(action), do: action.label
+  end
 end
--- learn_functional_programming_with_elixir/dungeon_crawl/lib/dungeon_crawl/room/room.ex
+++ learn_functional_programming_with_elixir/dungeon_crawl/lib/dungeon_crawl/room/room.ex
@@ -1,12 +1,19 @@
 defmodule DungeonCrawl.Room do
+  alias DungeonCrawl.Room.Triggers
   import DungeonCrawl.Room.Action
-  defstruct description: nil, actions: []
+  defstruct description: nil, actions: [], trigger: nil
 
   def all,
     do: [
       %__MODULE__{
-        description: "You found a quiet place. Looks safe for a little nap.",
-        actions: [forward(), rest()]
+        description: "You can see the light of day. You found the exit!",
+        actions: [forward()],
+        trigger: Triggers.Exit
+      },
+      %__MODULE__{
+        description: "You can see an enemy blocking your path.",
+        actions: [forward()],
+        trigger: Triggers.Enemy
       }
     ]
 end
