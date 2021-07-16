1f2645f8291404beb2401ada236c58daa03fa0e2
Alexandre Xavier <axavier@fullstacklabs.co>
2021-07-16 02:22:38 -0300

    Create add supervisors strategies to restard process

--- concurrent_data_processing_in_elixir/jobber/lib/jobber.ex
+++ concurrent_data_processing_in_elixir/jobber/lib/jobber.ex
@@ -1,18 +1,7 @@
 defmodule Jobber do
-  @moduledoc """
-  Documentation for `Jobber`.
-  """
+  alias Jobber.{JobRunner, JobSupervisor}
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Jobber.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start_job(args) do
+    DynamicSupervisor.start_child(JobRunner, {JobSupervisor, args})
   end
 end
