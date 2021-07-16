c8d29c1f58e1bd999b5551b58158839d538ea417
Alexandre Xavier <axavier@fullstacklabs.co>
2021-07-16 00:10:12 -0300

    Use Task.Supervisor and GenServer to send_mail

--- concurrent_data_processing_in_elixir/sender/lib/sender.ex
+++ concurrent_data_processing_in_elixir/sender/lib/sender.ex
@@ -16,6 +16,9 @@ defmodule Sender do
     :world
   end
 
+  def send_email("konnichiwa@world.com" = _email),
+    do: :error
+
   def send_email(email) do
     Process.sleep(3000)
     IO.puts("Email to #{email} sent")
@@ -23,12 +26,8 @@ defmodule Sender do
   end
 
   def notify_all(emails) do
-    emails
-    |> Enum.map(fn email ->
-      Task.async(fn ->
-        send_email(email)
-      end)
-    end)
-    |> Enum.map(&Task.await/1)
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1)
+    |> Enum.to_list()
   end
 end
